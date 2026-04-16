b0f3d56a2578742da9d04b98fc042647d6739c02
fenglin <1349410043@qq.com>
2026-04-17 03:05:32 +0800

    add local vllm script

---
+++ scripts/liar_raw_local_vllm.sh
@@ -0,0 +1,13 @@
+unset LD_PRELOAD
+export LD_LIBRARY_PATH="${CONDA_PREFIX}/lib:${LD_LIBRARY_PATH:-}"
+export TOKENIZERS_PARALLELISM=false
+export CUDA_VISIBLE_DEVICES="${CUDA_VISIBLE_DEVICES:-0,1,2,3}"
+unset NCCL_ALGO NCCL_PROTO
+export NCCL_CUMEM_HOST_ENABLE=0
+export NCCL_P2P_LEVEL=LOC
+
+python scripts/run_pipeline.py \
+  --config configs/liar_raw_local_vllm.yaml  \
+  --mode full \
+  --input data/converted/liar_raw_test.jsonl \
+  --output outputs/liar_raw_test_output.jsonl
